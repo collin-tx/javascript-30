ec61d81ccbc506b865c55c4ca5d32920783fbf0b
Collin Miller Smith <44204853+collin-tx@users.noreply.github.com>
2019-01-23 18:08:23 -0600

    Update scripts.js

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -25,16 +25,24 @@ function paintToCanvas() {
 
   return setInterval(() => {
     ctx.drawImage(video, 0, 0, width, height);
-    // take the pixels out
+   
     let pixels = ctx.getImageData(0, 0, width, height);
-    // mess with them
-    // pixels = redEffect(pixels);
 
     pixels = rgbSplit(pixels);
-    // ctx.globalAlpha = 0.8;
-
-    // pixels = greenScreen(pixels);
-    // put them back
+    
     ctx.putImageData(pixels, 0, 0);
   }, 16);
+}
+
+function takePhoto() {
+  // played the sound
+  snap.currentTime = 0;
+  snap.play();
+
+  const data = canvas.toDataURL('image/jpeg');
+  const link = document.createElement('a');
+  link.href = data;
+  link.setAttribute('download', 'handsome');
+  link.innerHTML = `<img src="${data}" alt="Handsome Man" />`;
+  strip.insertBefore(link, strip.firsChild);
 }
